09048a5a2f05f71ac8efce978fd60efa7bcee8cf
schmar00 <martin@schiegl.eu>
2024-02-08 18:28:09 +0100

    #test

--- js/pv.js
+++ js/pv.js
@@ -5,7 +5,7 @@ let USER_LANG = 'en';
 let BASE = location.protocol + '//' + location.host + location.pathname;
 
 $(document).ready(function () {
-document.getElementById('a').innerHTML = window.location;
+document.getElementById('a').innerHTML = window.location.href;
     
 
 });
